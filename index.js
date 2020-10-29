bb9c38760fa8e2d76dbdcfcf5482061ce051c658
dickm19 <maryclairedick@gmail.com>
2020-10-29 16:09:52 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+function submitData(name, email){
+       let formData = {
+        name: name,
+        email: email
+      };
+      
+      // method: "POST" is missing from the object below
+      let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+      
+      return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+            const h1 = document.createElement("h1")
+            
+            document.body.append(h1)
+            h1.textContent = object.id
+        })
+        .catch(function(error) {
+            alert("Bad things! Ragnarők!");
+            document.body.append(error.message);
+          })
+      
+      
+}
+
+//submitData("mary", "m@m.com")
